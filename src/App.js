17d8e615be34a82f196310cbd647168c0d7200f0
BabriWala <hanzalafarhann@gmail.com>
2022-08-24 15:52:34 +0600

    Navbar Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import React from 'react';
+import Navigation from './components/Navigation';
+
+const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <Navigation></Navigation>
+      <h2>OK</h2>
     </div>
   );
-}
+};
 
 export default App;
